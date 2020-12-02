e5c64e3e733af4d63ccde27f8b93655527fe850d
Josh Kennedy <joshk@digitalmagic.io>
2020-12-01 22:34:53 -0600

    Added challenge generator for auth

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const crypto = require('crypto');
+
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now());
@@ -10,6 +12,15 @@ router.use(function timeLog (req, res, next) {
 // define the home page route
 router.get('/', function (req, res) {
   res.send('Birds home page');
+});
+
+// Generates a code challenge and code verifier for authentication
+router.get('/pkce', function(req, res) {
+  const buf = Buffer.alloc(256);
+  const longRandomString = crypto.randomFillSync(buf).toString('hex');
+  const chunk = longRandomString.substring(Math.random * longRandomString.length - 129, 128);
+
+  res.send(chunk);
 });
 
 // define the about route

e538a20cd561be470adedf5094d8596e0ad205a8
Josh Kennedy <joshk@digitalmagic.io>
2020-12-11 22:12:35 -0600

    Optimized application_url secret

--- secrets.template.js
+++ secrets.template.js
@@ -4,9 +4,10 @@ let client_id = 'CLIENT ID GOES HERE!';
 let client_secret = 'CLIENT SECRET GOES HERE!';
 let development_url = 'http://localhost:3000';
 let production_url = 'PRODUCTION URL GOES HERE';
+const resolved_url = process.env.NODE_ENV !== 'production' ? development_url : production_url;
 
 module.exports = {
   client_id,
   client_secret,
-  application_url: process.env.NODE_ENV !== 'production' ? development_url : production_url
+  application_url: resolved_url
 };

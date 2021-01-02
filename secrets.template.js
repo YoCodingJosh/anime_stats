2ca445b68085422ed09a34be237048c9ad4b3046
Josh Kennedy <joshk@digitalmagic.io>
2021-01-01 18:48:27 -0600

    Put hint about environment variables in secret template

--- secrets.template.js
+++ secrets.template.js
@@ -7,6 +7,12 @@ let production_url = 'PRODUCTION URL GOES HERE';
 let application_environment = "DEV, STAGING, or PROD";
 const resolved_url = process.env.NODE_ENV !== 'production' ? development_url : production_url;
 
+// you can also set environment variables
+// MAL_CLIENT_ID = client_id
+// MAL_CLIENT_SECRET = client_secret
+// APP_URL = production_url or development_url depending on where you're running it
+// APP_ENV = application_environment
+
 module.exports = {
   client_id,
   client_secret,
